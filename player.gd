1ac82f08fdf715b2d3a1819b7fdea87145c368ef
Pedro Zuca <p.zuca@aluno.ifsp.edu.br>
2026-05-06 11:57:04 -0300

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480,720)
+
+func _process(delta: float) -> void:
+	velocity = Input.get_vector("ui_left","ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = 'Sprite run self made'
+	else:
+		$AnimatedSprite2D.animation = 'Sprite Idle self made'
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0 
+	
+func start():
+	set_process(true)
+	position = screensize /2
+	$AnimatedSprite2D.animation = 'Sprite Idle self made'
